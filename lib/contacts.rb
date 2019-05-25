d5c24a67595894195929534cfaf80b05bed81a73
Marc Poppy <poppy.marc@gmail.com>
2019-05-25 22:02:03 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,36 @@
 require 'pry'
 
-  # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
-  
-def remove_strawberry(contacts)
+  #This is the array we will be passing into the remove_strawberry method
+   contacts = {
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we",
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
 
+#def remove_strawberry(contacts)
+  #need to remove strawberry ice ice creams
+  #contacts.each do |name, data|
+      #data.each do |key, value|
+        #if key == :favorite_ice_cream_flavors
+        #  value.delete_if {|ice_cream| ice_cream == "strawberry"}
+        #end
+      #end
+  #end
+#end
+
+def remove_strawberry(contacts)
+  contacts.each do |name, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
